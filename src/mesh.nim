0c530cd89d497a1a9d6c5b74d45c61ab05e27b95
Christoph Pohl <christophpohl@gmx.com>
2020-10-27 11:04:28 +0100

    Read in dolfin XML meshes
    Only 2D triangles for now

--- src/mesh.nim
+++ src/mesh.nim
@@ -1,4 +1,4 @@
-import math
+import math, xmlparser, xmltree, strutils
 
 type
   Mesh* = object
@@ -23,3 +23,27 @@ proc UnitSquareMesh*(n: int): Mesh =
       result.connectivity.add([i + j*(n+1), i + (j+1)*(n+1), i + (j+1)*(n+1) + 1])
       result.connectivity.add([i + j*(n+1), i + (j+1)*(n+1) + 1, i + j*(n+1) + 1])
 
+proc readDolfinXml*(filename: string): Mesh =
+  let xml_contents = loadXml(filename)
+  let mesh_element = child(xml_contents, "mesh")
+  if mesh_element.attr("celltype") != "triangle":
+    quit("Celltype currently not supported.")
+
+  let vertices_element = child(mesh_element, "vertices")
+  let num_nodes = parseInt(vertices_element.attr("size"))
+  result.nodes = newSeq[array[2, float]](num_nodes)
+  for vertex in vertices_element.items():
+    let index = parseInt(vertex.attr("index"))
+    let x = parseFloat(vertex.attr("x"))
+    let y = parseFloat(vertex.attr("y"))
+    result.nodes[index] = [x, y]
+
+  let cells_element = child(mesh_element, "cells")
+  let num_elements = parseInt(cells_element.attr("size"))
+  result.connectivity = newSeq[array[3, int]](num_elements)
+  for cell in cells_element.items():
+    let index = parseInt(cell.attr("index"))
+    let v0 = parseInt(cell.attr("v0"))
+    let v1 = parseInt(cell.attr("v1"))
+    let v2 = parseInt(cell.attr("v2"))
+    result.connectivity[index] = [v0, v1, v2]
--- test/test_mesh.nim
+++ test/test_mesh.nim
@@ -9,3 +9,13 @@ block:
   let mesh = UnitSquareMesh(1)
   doAssert mesh.nodes == @[[0.0, 1.0], [1.0, 1.0], [0.0, 0.0], [1.0, 0.0]]
   doAssert mesh.connectivity == @[[0, 2, 3], [0, 3, 1]]
+
+block:
+  let mesh = readDolfinXml("data/ell_2d.xml")
+
+  doAssert mesh.nodes.len == 26
+  doAssert mesh.nodes[17][0] == 0.49609
+  doAssert mesh.nodes[17][1] == 1.29651
+
+  doAssert mesh.connectivity.len == 34
+  doAssert mesh.connectivity[7] == [8, 18, 7]
