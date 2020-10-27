556bca5da431e21f4a45ae6f87586ffdffa1c480
Christoph Pohl <christophpohl@gmx.com>
2020-10-27 15:59:08 +0100

    Make Mesh element type / dimension variable
    ... at compile time

--- src/mesh.nim
+++ src/mesh.nim
@@ -1,13 +1,13 @@
 import math, xmlparser, xmltree, strutils
 
 type
-  Mesh* = object
-    nodes*: seq[array[2, float]]
-    connectivity*: seq[array[3, int]]
+  Mesh*[Dim, NodesPerElement: static[int]]  = object
+    nodes*: seq[array[Dim, float]]
+    connectivity*: seq[array[NodesPerElement, int]]
     boundary_nodes*: seq[int]
 
 # division means number of elements per edge of the unit square
-proc UnitSquareMesh*(n: int): Mesh =
+proc UnitSquareMesh*(n: int): Mesh[2, 3] =
   result.nodes = newSeq[array[2, float]]((n + 1)^2)
   for i in 0..n:
     for j in 0..n:
@@ -23,27 +23,38 @@ proc UnitSquareMesh*(n: int): Mesh =
       result.connectivity.add([i + j*(n+1), i + (j+1)*(n+1), i + (j+1)*(n+1) + 1])
       result.connectivity.add([i + j*(n+1), i + (j+1)*(n+1) + 1, i + j*(n+1) + 1])
 
-proc readDolfinXml*(filename: string): Mesh =
+
+proc readDolfinXml*(D, N: static[int], filename: string): Mesh[D, N] =
   let xml_contents = loadXml(filename)
   let mesh_element = child(xml_contents, "mesh")
-  if mesh_element.attr("celltype") != "triangle":
+
+  let accetable_celltypes = ["triangle", "tetrahedron"]
+  if mesh_element.attr("celltype") notin accetable_celltypes:
     quit("Celltype currently not supported.")
 
   let vertices_element = child(mesh_element, "vertices")
   let num_nodes = parseInt(vertices_element.attr("size"))
-  result.nodes = newSeq[array[2, float]](num_nodes)
+  result.nodes = newSeq[array[D, float]](num_nodes)
   for vertex in vertices_element.items():
     let index = parseInt(vertex.attr("index"))
     let x = parseFloat(vertex.attr("x"))
     let y = parseFloat(vertex.attr("y"))
-    result.nodes[index] = [x, y]
+    when D == 2:
+      result.nodes[index] = [x, y]
+    elif D == 3:
+      let z = parseFloat(vertex.attr("z"))
+      result.nodes[index] = [x, y, z]
 
   let cells_element = child(mesh_element, "cells")
   let num_elements = parseInt(cells_element.attr("size"))
-  result.connectivity = newSeq[array[3, int]](num_elements)
+  result.connectivity = newSeq[array[N, int]](num_elements)
   for cell in cells_element.items():
     let index = parseInt(cell.attr("index"))
     let v0 = parseInt(cell.attr("v0"))
     let v1 = parseInt(cell.attr("v1"))
     let v2 = parseInt(cell.attr("v2"))
-    result.connectivity[index] = [v0, v1, v2]
+    when N == 3:
+      result.connectivity[index] = [v0, v1, v2]
+    elif N == 4:
+      let v3 = parseInt(cell.attr("v3"))
+      result.connectivity[index] = [v0, v1, v2, v3]
--- test/test_mesh.nim
+++ test/test_mesh.nim
@@ -11,7 +11,7 @@ block:
   doAssert mesh.connectivity == @[[0, 2, 3], [0, 3, 1]]
 
 block:
-  let mesh = readDolfinXml("data/ell_2d.xml")
+  let mesh = readDolfinXml(2, 3, "data/ell_2d.xml")
 
   doAssert mesh.nodes.len == 26
   doAssert mesh.nodes[17][0] == 0.49609
@@ -19,3 +19,13 @@ block:
 
   doAssert mesh.connectivity.len == 34
   doAssert mesh.connectivity[7] == [8, 18, 7]
+
+block:
+  let mesh = readDolfinXml(3, 4, "data/beam.xml")
+
+  doAssert mesh.nodes.len == 176
+  doAssert mesh.nodes[17][0] == 6.0e-1
+  doAssert mesh.nodes[17][1] == 2.0e-1/3.0
+
+  doAssert mesh.connectivity.len == 540
+  doAssert mesh.connectivity[7] == [1, 2, 46, 57]
